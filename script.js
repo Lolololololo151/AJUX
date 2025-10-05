017547cb38e8a4e7dbee2362c5b1015741143d47
Roma <esetantivatorz1@gmail.com>
2025-10-05 19:32:16 +0300

    Added Captian of blablaland

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-fetch(URL, options)
+let heroJSON = `{"name": "Captain of blablaLand",
+                "age": 21,
+                "power": "LooksMaxxing"}`;
+
+let hero = JSON.parse(heroJSON);
+
+let heroElement = document.getElementById("hero");
+
+heroElement.innerHTML = `<h1>${hero.name}</h1>
+                        <p>Age: ${hero.age}</p>
+                        <p>Super power: ${hero.power}</p>`
+
