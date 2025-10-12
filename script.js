a80958f177ad4bf21c904332a58a52edc1ab1898
Roma <esetantivatorz1@gmail.com>
2025-10-12 18:39:55 +0300

    The end

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-let heroJSON = `{"name": "Captain of blablaLand",
-                "age": 21,
-                "power": "LooksMaxxing"}`;
+// let heroJSON = `{"name": "Captain of blablaLand",
+//                 "age": 21,
+//                 "power": "LooksMaxxing"}`;
 
-let hero = JSON.parse(heroJSON);
+// let hero = JSON.parse(heroJSON);
 
-let heroElement = document.getElementById("hero");
+// let heroElement = document.getElementById("hero");
 
-heroElement.innerHTML = `<h1>${hero.name}</h1>
-                        <p>Age: ${hero.age}</p>
-                        <p>Super power: ${hero.power}</p>`
+// heroElement.innerHTML = `<h1>${hero.name}</h1>
+//                         <p>Age: ${hero.age}</p>
+//                         <p>Super power: ${hero.power}</p>`
 
+let url = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
+
+fetch(url)
+    .then(async function(response) {
+        let data = await response.json()
+        console.log(data)
+    })
